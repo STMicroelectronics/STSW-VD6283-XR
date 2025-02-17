dfca36b5678b810826c7d3f98f580be34411dc4f
Philippe Legeard <philippe.legeard@st.com>
2025-02-16 22:24:45 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # STSW-VD6283-XR
 Linux kernel driver for VD6283 for xr2gen3 Qualcomm reference design platform
+
+This linux kernel driver implements a bridge that conveys SPI data in a context of a wider software package.
+This software package implements the 3 features the VD6283 device is capable of : cci, lux and flicker detection. 
+
+The following diagram describes a software overview. 
+
+![](./png/sw_overview.png)
+
+The VD6283 is connected through I2C to the sensor hub processor. 
+The sensor hub processor then controls the device (initialization, start, stop) and calculates lux and cct from the light counters information extracted from the i2c channel.
+It also sends the START and STOP commands to control the flicker channel.
+
+The clear channel of the vd628x device is used to detect and extract the frequency of the ambient light
+As described in the figure above the SPI bus of the processor to which the VD6283 device is connected to, gets PDM data from GPIO1 at the same frequency of the SPI bus.
+
+![](./png/flicker_detect.png)
+
---
+++ vd628x_spi.c
@@ -0,0 +1,528 @@
+/******************** (C) COPYRIGHT 2025 STMicroelectronics ********************
+*
+* File Name          : vd628x_spi.c
+*
+********************************************************************************
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* THE PRESENT SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES
+* OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, FOR THE SOLE
+* PURPOSE TO SUPPORT YOUR APPLICATION DEVELOPMENT.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+*******************************************************************************/
+
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/regulator/consumer.h>
+#include <linux/miscdevice.h>
+
+#include "cam_sensor_dev.h"
+#include "vd628x_spi_ioctl.h"
+
+#include <linux/of.h>
+#include <linux/uaccess.h>
+
+#define VD628x_ADAPTER_DEV_NAME "vd628x_adapter"
+
+struct regulator_info {
+	struct regulator *reg;
+	const char *name;
+};
+
+struct vd628x_spidev_data {
+	struct spi_device *pdev;
+	struct miscdevice misc;
+	struct cam_hw_soc_info soc_info;
+	uint32_t number_of_regulators;
+	struct regulator_info regulators[1];
+	uint32_t min_volt[1];
+	uint32_t max_volt[1];
+	uint32_t load_current[1];
+
+	u8 *pbuffer;
+	int16_t *psamples;
+	u32 spi_max_frequency;
+	u32 spi_buffer_size;
+	u32 spi_speed_hz;
+	u16 samples_nb_per_chunk;
+	u16 pdm_data_sample_width_in_bytes;
+};
+
+// legacy chunk transfer function. To be used by user part as a backup
+static ssize_t vd628x_spi_read(struct file *file, char __user *buf, size_t count, loff_t *f_pos)
+{
+	int status = 0;
+	unsigned long missing;
+	struct vd628x_spidev_data *pdata = container_of(file->private_data,
+		struct vd628x_spidev_data, misc);
+
+	struct spi_transfer t = {
+			.rx_buf = pdata->pbuffer,
+			.len		= count,
+		};
+	struct spi_message m;
+
+	if (count > pdata->spi_buffer_size)
+		return -EMSGSIZE;
+
+	// set the speed set by user, or the max_frequency one if not set by user
+	t.speed_hz = pdata->spi_speed_hz;
+
+	spi_message_init(&m);
+	spi_message_add_tail(&t, &m);
+	status = spi_sync(pdata->pdev, &m);
+	if (status != 0) {
+		pr_err("[%d] spi read failed\n", status);
+		return status;
+	}
+
+	status = m.actual_length;
+	missing = copy_to_user(buf, pdata->pbuffer, status);
+	if (missing == status)
+		status = -EFAULT;
+	else
+		status = status - missing;
+
+	return status;
+}
+
+
+static int vd628x_spi_open(struct inode *inode, struct file *file)
+{
+	struct vd628x_spidev_data *pdata = container_of(file->private_data,
+		struct vd628x_spidev_data, misc);
+
+	if (!pdata->pbuffer) {
+
+		if (pdata->spi_buffer_size != 0) {
+			pdata->pbuffer = kmalloc(pdata->spi_buffer_size, GFP_KERNEL);
+			if (!pdata->pbuffer) {
+				pr_err("[%s] vd628x open spi failed => ENOMEM", __func__);
+				return -ENOMEM;
+			}
+		}
+		else
+			return -EFAULT;
+	}
+
+	return 0;
+}
+
+static int vd628x_spi_chunk_transfer_and_get_samples(struct vd628x_spidev_data *pdata)
+{
+	int i,s;
+	uint16_t index;
+	uint32_t d;
+
+	int status = 0;
+
+	struct spi_transfer t = {
+			.rx_buf = pdata->pbuffer,
+			.len	= pdata->spi_buffer_size,
+		};
+	struct spi_message m;
+
+
+	// set the speed set by user, or the max_frequency one if not set by user
+	t.speed_hz = pdata->spi_speed_hz;
+
+	spi_message_init(&m);
+	spi_message_add_tail(&t, &m);
+	status = spi_sync(pdata->pdev, &m);
+	if (status != 0) {
+		pr_err("[%d] spi read failed\n", status);
+		return status;
+	}
+
+	if (m.actual_length != pdata->spi_buffer_size) {
+		printk("vd628x. spi tranfser error");
+		return -EFAULT;
+	}
+
+	for(s = 0; s < pdata->samples_nb_per_chunk ; s++) {
+		// example : SPI frequency = 5*1024*1204 Hz. sampling rate = 2048.
+		// ==> pdm_sample_width in bytes = 320 = 8*40
+		// i = 0,8,16, ..... 312. ==> each __builtin_popcountll is applied on 8 bytes = 64 bits
+		pdata->psamples[s] = 0;
+		index = s*pdata->pdm_data_sample_width_in_bytes;
+		for(i = 0; i < pdata->pdm_data_sample_width_in_bytes; i += 4) {
+			d = 0;
+			d += (((uint32_t)pdata->pbuffer[index+i])&0xFF);
+			d += (((uint32_t)pdata->pbuffer[index+i+1])&0xFF)<<8;
+			d += (((uint32_t)pdata->pbuffer[index+i+2])&0xFF)<<16;
+			d += (((uint32_t)pdata->pbuffer[index+i+3])&0xFF)<<24;
+			pdata->psamples[s] +=  __builtin_popcountl(d);
+		}
+	}
+
+	return 0;
+}
+
+
+static int vd628x_spi_ioctl_handler(struct vd628x_spidev_data *pdata, unsigned int cmd, unsigned long arg)
+{
+	int ret;
+	struct vd628x_spi_info spi_info;
+	struct vd628x_spi_params spi_params;
+
+	if (!pdata)
+		return -EINVAL;
+
+	switch (cmd) {
+		case VD628x_IOCTL_GET_SPI_INFO:
+			spi_info.chunk_size = pdata->spi_buffer_size;
+			spi_info.spi_max_frequency = pdata->spi_max_frequency;
+			ret = copy_to_user((void __user *) arg, &spi_info, sizeof(struct vd628x_spi_info));
+			break;
+
+		case VD628x_IOCTL_SET_SPI_PARAMS:
+			ret = copy_from_user(&spi_params, (void __user *)arg, sizeof(struct vd628x_spi_params));
+			if (ret != 0)
+				return ret;
+			if ((!spi_params.speed_hz) || (!spi_params.samples_nb_per_chunk) || (!spi_params.pdm_data_sample_width_in_bytes))
+				return -EINVAL;
+			pdata->spi_speed_hz = spi_params.speed_hz;
+			pdata->samples_nb_per_chunk = spi_params.samples_nb_per_chunk;
+			pdata->pdm_data_sample_width_in_bytes = spi_params.pdm_data_sample_width_in_bytes;
+			if (pdata->psamples)
+				kfree(pdata->psamples);
+			pdata->psamples = (int16_t *)kmalloc(pdata->samples_nb_per_chunk*sizeof(int16_t), GFP_KERNEL);
+			printk("vd628x : spi speed set : %d", pdata->spi_speed_hz);
+			printk("vd628x : nb of samples per chunk  : %d", pdata->samples_nb_per_chunk);
+			printk("vd628x : sample width in bytes: %d", pdata->pdm_data_sample_width_in_bytes);
+			break;
+
+		case VD628x_IOCTL_GET_CHUNK_SAMPLES:
+			ret = vd628x_spi_chunk_transfer_and_get_samples(pdata);
+			if (ret != 0)
+				return ret;
+			ret = copy_to_user((void __user *) arg, pdata->psamples, pdata->samples_nb_per_chunk * sizeof(int16_t));
+			break;
+
+		default:
+			ret = -EINVAL;
+	}
+
+	return ret;
+}
+
+static long vd628x_spi_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+
+	struct vd628x_spidev_data *pdata = container_of(file->private_data,
+		struct vd628x_spidev_data, misc);
+
+return vd628x_spi_ioctl_handler(pdata, cmd, arg);
+}
+
+static int vd628x_spi_release(struct inode *inode, struct file *file)
+{
+	struct vd628x_spidev_data *pdata = container_of(file->private_data,
+		struct vd628x_spidev_data, misc);
+
+	if (pdata->pbuffer) {
+		kfree(pdata->pbuffer);
+		pdata->pbuffer = NULL;
+	}
+
+	if (pdata->psamples) {
+		kfree(pdata->psamples);
+		pdata->psamples = NULL;
+	}
+
+	return 0;
+}
+
+static int vd628x_spi_parse_dt(struct vd628x_spidev_data *pdata)
+{
+	int ret = 0;
+	struct device_node *of_node = pdata->pdev->dev.of_node;
+
+	ret = of_property_read_u32(of_node, "spi-max-frequency",&pdata->spi_max_frequency);
+	if (ret) {
+		pr_err("[%s] failed to read spi-frequency", __func__);
+		return ret;
+	}
+
+	// set the spi speed to max frequency by default
+	pdata->spi_speed_hz = pdata->spi_max_frequency;
+
+	printk("vd628x : [%s] spi (max) frequency=%d", __func__, pdata->spi_max_frequency);
+
+	ret = of_property_read_u32(of_node, "chunk-size",&pdata->spi_buffer_size);
+	if (ret) {
+		pr_err("[%s] failed to read spi-frequency", __func__);
+		return ret;
+	}
+
+	// set the spi speed to max frequency by default
+	printk("vd628x : [%s] spi chunk size=%d", __func__, pdata->spi_buffer_size);
+
+	return 0;
+}
+
+static const struct file_operations vd628x_spi_fops = {
+	.owner		= THIS_MODULE,
+	.open		= vd628x_spi_open,
+	.release	= vd628x_spi_release,
+	.read		= vd628x_spi_read,
+	.unlocked_ioctl	= vd628x_spi_ioctl
+};
+
+
+static int vd628x_get_ext_regulators(struct vd628x_spidev_data *pdata)
+{
+	int count = 0, i = 0, ret = 0;
+	struct device_node *of_node = pdata->soc_info.dev->of_node;
+
+	pr_info("vd628x: getting regulators info\n");
+
+	count = of_property_count_strings(of_node, "regulator-names");
+	if (count != -EINVAL) {
+		if (count <= 0) {
+			pr_err("vd628x : No regulator found\n");
+			pdata->number_of_regulators = 0;
+			return -1;
+		}
+		pdata->number_of_regulators = count;
+	} else {
+		pr_info("vd628x: No regulator node found\n");
+		return 0;
+
+	}
+
+	for (i = 0; i < count; i++) {
+		ret = of_property_read_string_index(of_node,
+			"regulator-names", i, &pdata->regulators[i].name);
+		if (ret) {
+			pr_err("vd628x: Invalid argument: No reg name found");
+			return -1;
+		}
+		printk("vd628x: regulator: %s found at index: %d\n",
+			pdata->regulators[i].name, i);
+	}
+
+	ret = of_property_read_u32_array(of_node, "rgltr-min-voltage",
+		pdata->min_volt, pdata->number_of_regulators);
+	if (ret) {
+		pr_err("vd628x: Min volt Array not found\n");
+		goto clear_reg_array;
+	}
+
+	ret = of_property_read_u32_array(of_node, "rgltr-max-voltage",
+		pdata->max_volt, pdata->number_of_regulators);
+	if (ret) {
+		pr_err("vd628x: Max volt Array not found\n");
+		goto clear_min_volt_array;
+	}
+
+	ret = of_property_read_u32_array(of_node, "rgltr-load-current",
+		pdata->load_current, pdata->number_of_regulators);
+	if (ret) {
+		pr_err("vd628x: Load current Array not found\n");
+		goto clear_max_volt_array;
+	}
+	return 0;
+
+clear_max_volt_array:
+	for (i = 0; i < count; i++)
+		pdata->max_volt[i] = 0;
+clear_min_volt_array:
+	for (i = 0; i < count; i++)
+		pdata->min_volt[i] = 0;
+clear_reg_array:
+	for (i = 0; i < count; i++)
+		pdata->regulators[i].name = NULL;
+
+	return ret;
+}
+
+
+
+
+static int vd628x_enable_ext_regulators(struct vd628x_spidev_data *pdata) {
+
+	int ret = 0, i = 0;
+
+	pr_info("vd628x: enabling regulators info\n");
+
+	for (i = 0; i < pdata->number_of_regulators; i++) {
+		pdata->regulators[i].reg = regulator_get(pdata->soc_info.dev, pdata->regulators[i].name);
+		if (IS_ERR_OR_NULL(pdata->regulators[i].reg)) {
+			if(PTR_ERR(pdata->regulators[i].reg))
+				pr_err("vd628x: NULL pointer");
+			ret = -EINVAL;
+			pr_err("vd628x: reg: %s failed ret: %d\n", pdata->regulators[i].name, ret);
+			goto error;
+		}
+
+		printk("vd628x: reg_name[%d]:%s: min_volt[%d]:%d, max_volt[%d]:%d load_current[%d]:%d\n", i, pdata->regulators[i].name, i, pdata->min_volt[i], i, pdata->max_volt[i], i, pdata->load_current[i]);
+
+		ret = regulator_set_voltage(pdata->regulators[i].reg, pdata->min_volt[i], pdata->max_volt[i]);
+		if (ret) {
+			pr_err("vd628x: %s set voltage failed ret: %d", pdata->regulators[i].name, ret);
+			goto error;
+		}
+
+		ret = regulator_set_load(pdata->regulators[i].reg, pdata->load_current[i]);
+		if (ret) {
+			pr_err("vd628x: %s set load failed ret: %d", pdata->regulators[i].name, ret);
+			regulator_set_voltage(pdata->regulators[i].reg, 0, pdata->max_volt[i]);
+			goto error;
+		}
+
+		ret = regulator_enable(pdata->regulators[i].reg);
+		if (ret) {
+			pr_err("vd628x: %s regulator_enable failed ret: %d", pdata->regulators[i].name, ret);
+			regulator_set_load(pdata->regulators[i].reg, 0);
+			regulator_set_voltage(pdata->regulators[i].reg, 0, pdata->max_volt[i]);
+			goto error;
+		}
+	}
+	return 0;
+
+error:
+	for (i -= 1; i >= 0; i--) {
+		regulator_set_voltage(pdata->regulators[i].reg, 0, pdata->max_volt[i]);
+		regulator_set_load(pdata->regulators[i].reg, 0);
+		regulator_put(pdata->regulators[i].reg);
+		pdata->regulators[i].reg = NULL;
+	}
+	return ret;
+}
+
+
+
+int vd628x_spi_driver_probe(struct spi_device *pdev)
+{
+	int ret = 0;
+	struct vd628x_spidev_data *pdata;
+
+	pr_info("vd628x: enter probe spi\n");
+
+
+	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
+	if (!pdata)
+		return -ENOMEM;
+
+	pdata->pdev = pdev;
+	spi_set_drvdata(pdev, pdata);
+
+	vd628x_spi_parse_dt(pdata);
+
+	//pr_info("[%s] spi mode=%d, cs=%d, bits_per_word=%d, speed=%d, csgpio=%d, modalias=%s",
+	//	___func__, pdev->mode, pdev->chip_select, pdev->bits_per_word,
+	//	pdev->max_speed_hz, pdev->cs_gpio, pdev->modalias);
+
+	pdata->misc.minor = MISC_DYNAMIC_MINOR;
+	pdata->misc.name = "vd628x_spi";
+	pdata->misc.fops = &vd628x_spi_fops;
+	ret = misc_register(&pdata->misc);
+
+	ret = vd628x_get_ext_regulators(pdata);
+	if (ret) {
+		pr_err("vd628x_get_ext_regulators => %d", ret);
+		return ret;
+	}
+
+	ret = vd628x_enable_ext_regulators(pdata);
+	if (ret) {
+		pr_err("vd628x_enable_ext_regulators failed => %d", ret);
+		return ret;
+	}
+
+
+	if (ret)
+		pr_info("vd628x_spi_probe failed");
+	else
+		pr_info("vd628x_spi_probe successfully");
+
+	return ret;
+}
+
+
+int vd628x_spi_driver_remove(struct spi_device *pdev)
+{
+	struct vd628x_spidev_data *pdata;
+	int i;
+
+	pdata = spi_get_drvdata(pdev);
+	if (!pdata) {
+		pr_err("[%s] can't remove %p", __func__, pdev);
+		return 0;
+	}
+
+	for (i = 0; i < pdata->number_of_regulators; i++) {
+		regulator_set_voltage(pdata->regulators[i].reg, 0, pdata->max_volt[i]);
+		regulator_set_load(pdata->regulators[i].reg, 0);
+		regulator_disable(pdata->regulators[i].reg);
+		regulator_put(pdata->regulators[i].reg);
+		pdata->regulators[i].reg = NULL;
+	}
+
+
+	misc_deregister(&pdata->misc);
+
+	return 0;
+}
+
+
+
+static const struct of_device_id vd628x_adapter_dt_ids[] = {
+	{ .compatible = "st,vd628x_adapter" },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, vd628x_adapter_dt_ids);
+
+static const struct of_device_id vd628x_spi_dt_ids[] = {
+	{ .compatible = "st,vd628x_spi" },
+	{ /* sentinel */ }
+};
+
+static struct spi_driver vd628x_spi_driver = {
+ 	.driver = {
+		.name = "vd628x_spi",
+		.owner = THIS_MODULE,
+		.of_match_table = vd628x_spi_dt_ids,
+ 	},
+	.probe = vd628x_spi_driver_probe,
+	.remove = vd628x_spi_driver_remove,
+};
+
+MODULE_DEVICE_TABLE(of, vd628x_spi_dt_ids);
+
+static int __init vd628x_module_init(void)
+{
+	int ret = 0;
+
+	pr_info("vd628x: module init\n");
+
+	ret = spi_register_driver(&vd628x_spi_driver);
+	if (ret < 0) {
+		pr_err("spi_register_driver failed => %d", ret);
+		return ret;
+	}
+
+	return ret;
+}
+
+static void __exit vd628x_module_exit(void)
+{
+
+	pr_debug("vd628x : module exit\n");
+
+	spi_unregister_driver(&vd628x_spi_driver);
+}
+
+module_init(vd628x_module_init);
+module_exit(vd628x_module_exit);
+
+MODULE_DESCRIPTION("vd628x spi adapter driver");
+MODULE_LICENSE("GPL v2");
